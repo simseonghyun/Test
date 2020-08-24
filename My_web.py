213fb0a04310e0875fa76757d6600b4daba03fdc
simseonghyun <19352@naver.com>
2020-08-24 15:08:04 +0900

    Update My_web.py

--- My_web.py
+++ My_web.py
@@ -23,12 +23,66 @@ def render_file():
     global detector
     global model
 
-
+    if detector == None :
+        detector = MTCNN()
+        
     return render_template('upload.html')
 
-@app.route('/file_uploaded')
+@app.route('/file_uploaded', methods = ['GET', 'POST'])
 def upload_file():
-    return render_template('fail_back.html')
+    if request.method == 'POST': # POST 방식으로 전달된 경우
+        f = request.files['upload_image'].read()
+        # # 파일 객체 혹은 파일 스트림을 가져오고, html 파일에서 넘겨지는 값의 이름을 file1으로 했기 때문에 file1임.
+        # 업로드된 파일을 특정 폴더에저장하고,
+
+        # convert string data to numpy array
+        npimg = numpy.fromstring(f, dtype = numpy.uint8)
+        # convert numpy array to image
+        img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+        import face_detection
+        import predict
+        global detector
+        face_extract = face_detection.input_image(detector,img)
+        print("얼굴추출 완료")
+        if len(face_extract) == 0 :
+            print("얼굴인식 못했음")
+            return render_template('fail_back.html')
+        else :
+            # cv2.imshow('original', face_extract)
+            # cv2.waitKey(0)
+            #
+            # cv2.imwrite('face_test.jpg', face_extract)
+            #
+            #
+            # img = Image.fromarray(face_extract)
+            # print("fromarray")
+            # #BGR - > RGB 블루끼 없애줌
+            # b, g, r = img.split()
+            # img = Image.merge("RGB", (r, g, b))
+            # # img.save("temp.jpeg")
+            #
+            # # create file-object in memory
+            # file_object = io.BytesIO()
+            #
+            # img.save(file_object, 'JPEG')
+            #
+            # # move to beginning of file so `send_file()` it will read from start
+            # file_object.seek(0)
+
+            global model
+            result,k= predict.prediction(face_extract,model)
+
+            iu_percent = round(float(k[0][0]*100),3)
+            suzy_percent = round(float(k[0][1])*100,3)
+
+            # return send_file(file_object, mimetype='image/jpeg')
+            if iu_percent > suzy_percent :
+                return render_template('result.html',image_file = "image/result_iu.jpg", not_similler = "수지", not_similler_percent = suzy_percent, similler = "아이유", similler_percent = iu_percent  )
+            else :
+                return render_template('result.html', image_file="image/result_suzy.jpg", not_similler = "아이유", not_similler_percent = iu_percent, similler = "수지", similler_percent = suzy_percent )
+    else:
+        return render_template('fail_back.html')
 if __name__ == '__main__':
     # debug를 True로 세팅하면, 해당 서버 세팅 후에 코드가 바뀌어도 문제없이 실행됨.
     app.run(host='127.0.0.1', debug = True, threaded = True)
